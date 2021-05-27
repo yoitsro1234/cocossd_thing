78abc5798d7abfa2aef5c7beee7c39ec36af5abf
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-05-27 14:36:53 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 img  = "";
 status = "";
+objects = [];
 
 function preload(){
     img = loadImage("dog_cat.jpg")
 }
 
 function setup(){
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();    
+
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML= "Status: Detecting Objects"
 }
@@ -15,21 +21,22 @@ function setup(){
 function modelLoaded(){
     console.log("Model Loaded, p o g");
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results){
     if (error){
         console.log(error);
-    }else{
+    } else {
         console.log(results);
+        objects = results;
     }
 }
 
 
 function draw(){
-    image(img, 0, 0, 640, 420);
-    fill("#FF0000");
+    image(video, 0, 0, 380, 380);
+    /*fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
     stroke("#FF0000");
@@ -39,6 +46,25 @@ function draw(){
     text("Cat", 320, 420);
     noFill();
     stroke("#FF0000");
-    rect(300, 90, 270, 320);
-}
+    rect(300, 90, 270, 320);*/
 
+    if (status != ""){
+        objectDetector.detect(video, gotResult);
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        for (i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected is: "+objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%" + objects[i].x, objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+} 
